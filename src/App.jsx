9d02a6559baf0cfada7a791ca5c65d6dff873e57
Shubh Arya <shubh.arya37@gmail.com>
2025-05-26 21:31:29 +0530

    added new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/Home'
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/Home';
+import ProfilePage from './pages/Profile';
+import LoginPage from './pages/Login';
+import SignupPage from './pages/Signup';
+import RequiresAuth from './components/Auth';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      {/* <Route path="/profile" element={<Profile />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Signup />} /> */}
+      <Route
+        path="/profile"
+        element={
+          <RequiresAuth>
+            <ProfilePage />
+          </RequiresAuth>
+        }
+      />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<SignupPage />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
